721b6b1069c0e72a35a11d4f623f74df07c154d7
Jordan Graves <Jordang3177@gmail.com>
2020-02-20 01:30:40 +0800

    Added more to Room 5

--- Rooms/room_5.py
+++ Rooms/room_5.py
@@ -9,4 +9,5 @@ class Room5:
         pass
 
     def room_5(self):
-        print("Work in Progress")
+        pygame.mixer.Channel(4).play(Sounds.door_closing(self))
+        print("You enter into a room with nothing in it but two doors and a piano in the corner of the room")
